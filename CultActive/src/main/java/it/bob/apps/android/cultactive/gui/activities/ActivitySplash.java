4f985ba9115b573a01e254174d9c5e0202d3732e
debianbob <a@b.c>
2015-10-11 23:16:43 +0200

    inserita splash activity

--- CultActive/src/main/java/it/bob/apps/android/cultactive/gui/activities/ActivitySplash.java
+++ CultActive/src/main/java/it/bob/apps/android/cultactive/gui/activities/ActivitySplash.java
@@ -1,7 +1,42 @@
 package it.bob.apps.android.cultactive.gui.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import it.bob.apps.android.cultactive.R;
+
 /**
  * Created by roberto on 6/19/15.
  */
-public class ActivitySplash {
+public class ActivitySplash extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(ActivitySplash.this, ActivityMain.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
 }
